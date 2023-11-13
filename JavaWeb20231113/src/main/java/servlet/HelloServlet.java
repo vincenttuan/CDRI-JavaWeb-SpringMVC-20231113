141a2babe25e37e0108192b4cabcc195259c71f5
aweit <aa4192696@gmail.com.tw>
2023-11-13 15:20:20 +0800

    java

---
+++ JavaWeb20231113/src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/servlet/hello")
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 編碼
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		
+		String name = "John";
+		
+		// 回應
+		PrintWriter out = resp.getWriter();
+		out.println("哈囉 ! " + name);
+		
+	}
+	
+	
+	
+}

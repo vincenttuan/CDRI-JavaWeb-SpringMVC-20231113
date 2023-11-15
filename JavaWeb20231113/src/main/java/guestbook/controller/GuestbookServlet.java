fe3e173463a6b32f7806706bbbb2fb8a1fd99a33
aweit <aa4192696@gmail.com.tw>
2023-11-15 12:26:59 +0800

    java

--- JavaWeb20231113/src/main/java/guestbook/controller/GuestbookServlet.java
+++ JavaWeb20231113/src/main/java/guestbook/controller/GuestbookServlet.java
@@ -1,6 +1,7 @@
 package guestbook.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,14 +10,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import guestbook.model.Guestbook;
+import guestbook.model.GuestbookDao;
+import guestbook.model.GuestbookDaoInMemory;
+
 @WebServlet("/guestbook")
 public class GuestbookServlet extends HttpServlet {
-
+	private GuestbookDao guestbookDao = new GuestbookDaoInMemory();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 取得所有留言紀錄
+		List<Guestbook> guestbooks = guestbookDao.readAll();
 		
 		// 重導到 /WEB-INF/view/guestbook/guestbook.jsp
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/guestbook/guestbook.jsp");
+		req.setAttribute("guestbooks", guestbooks); // 傳遞參數給 jsp
 		rd.forward(req, resp);
 	}
 	

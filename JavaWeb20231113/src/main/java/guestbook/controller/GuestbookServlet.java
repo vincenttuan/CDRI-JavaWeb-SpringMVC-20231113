4939ec0e33aed7d65bce6d0191c706fb60722c0c
aweit <aa4192696@gmail.com.tw>
2023-11-15 10:52:02 +0800

    java

--- JavaWeb20231113/src/main/java/guestbook/controller/GuestbookServlet.java
+++ JavaWeb20231113/src/main/java/guestbook/controller/GuestbookServlet.java
@@ -1,5 +1,23 @@
 package guestbook.controller;
 
-public class GuestbookServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/guestbook")
+public class GuestbookServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 重導到 /WEB-INF/view/guestbook/guestbook.jsp
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/guestbook/guestbook.jsp");
+		rd.forward(req, resp);
+	}
+	
 }

acbf4b66d51fcb9eb56c136a31b655f3701f3ac1
aweit <aa4192696@gmail.com.tw>
2023-11-20 10:07:54 +0800

    java

---
+++ JavaWeb20231113/src/main/java/filter/UTF8EncodingFilter.java
@@ -0,0 +1,31 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// UTF-8 過濾器
+@WebFilter(value = {"/*"})
+public class UTF8EncodingFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		
+		//res.getWriter().print("stop !");
+		// 設定 utf-8
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=utf-8");
+		
+		// 放行
+		chain.doFilter(req, resp);
+		
+	}
+	
+}

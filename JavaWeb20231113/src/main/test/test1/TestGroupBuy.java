d364a7acabb8163dda143ec745d5b520a30a227b
aweit <aa4192696@gmail.com.tw>
2023-11-21 12:39:01 +0800

    java

--- JavaWeb20231113/src/main/test/test1/TestGroupBuy.java
+++ JavaWeb20231113/src/main/test/test1/TestGroupBuy.java
@@ -1,7 +1,11 @@
 package test1;
 
+import java.util.ArrayList;
+
 import group_buy.dao.GroupBuyDao;
 import group_buy.dao.GroupbuyDaoInMemory;
+import group_buy.entity.Cart;
+import group_buy.entity.CartItem;
 import group_buy.entity.Product;
 import group_buy.entity.User;
 
@@ -23,9 +27,32 @@ public class TestGroupBuy {
 		dao.addProduct(product2);
 		dao.addProduct(product3);
 		
+		// 加入購物車
+		Cart cart1 = new Cart(301, 101, false);
+		cart1.setUser(user1); // 設定關聯
+		CartItem item1 = new CartItem(401, 301, 201, 10);
+		CartItem item2 = new CartItem(402, 301, 202, 15);
+		cart1.setCartItems(new ArrayList<CartItem>()); // 設定 CartItems 集合, 用來放多筆 CartItem
+		cart1.getCartItems().add(item1); // 將 CartItem 放入到 cart 的 CartItems 集合中
+		cart1.getCartItems().add(item2); // 將 CartItem 放入到 cart 的 CartItems 集合中
+		
+		dao.addCart(cart1);
+		
 		System.out.println(dao.findAllUsers());
 		System.out.println(dao.findAllProducts());
 		System.out.println(dao.findAllCarts());
+		
+		System.out.println("-------------------------------");
+		Cart cart = dao.findNoneCheckoutCartByUserId(101).get();
+		System.out.printf("user id: %d username: %s\n", cart.getUserId(), cart.getUser().getUsername());
+		for(CartItem item : cart.getCartItems()) {
+			//System.out.printf("Id: %d 商品ID: %d 數量: %d \n", item.getItemId(), item.getProductId(), item.getQuantity());
+			System.out.printf("Id: %d 商品ID: %d 商品名稱: %s 數量: %d \n", 
+					item.getItemId(), 
+					item.getProductId(), 
+					item.getProduct() == null ? "null" : item.getProduct().getProductName(), 
+					item.getQuantity());
+		}
 		
 		
 	}

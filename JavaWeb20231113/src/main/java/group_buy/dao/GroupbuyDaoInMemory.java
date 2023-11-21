e448f85dff1eb5fc7f76cb7649cea488adf2d46d
aweit <aa4192696@gmail.com.tw>
2023-11-21 11:21:42 +0800

    java

--- JavaWeb20231113/src/main/java/group_buy/dao/GroupbuyDaoInMemory.java
+++ JavaWeb20231113/src/main/java/group_buy/dao/GroupbuyDaoInMemory.java
@@ -49,25 +49,22 @@ public class GroupbuyDaoInMemory implements GroupBuyDao {
 
 	@Override
 	public Optional<User> findUserByName(String username) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		return users.stream().filter(user -> user.getUsername().equals(username)).findAny();
 	}
 
 	@Override
 	public Optional<User> findUserById(Integer userId) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		return users.stream().filter(user -> user.getUserId().equals(userId)).findAny();
+		//return users.stream().filter(user -> user.getUserId().intValue() == userId.intValue()).findAny();
 	}
 
 	@Override
 	public List<Product> findAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		return products;
 	}
 
 	@Override
 	public Optional<Product> findProductById(Integer productId) {
-		// TODO Auto-generated method stub
 		return Optional.empty();
 	}
 

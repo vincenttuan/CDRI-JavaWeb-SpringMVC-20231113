473be01f11cbf17100369fa8f77eb5fce44a022d
aweit <aa4192696@gmail.com.tw>
2023-12-04 13:53:38 +0800

    java

--- SpringCore/src/session01/test/TestHello.java
+++ SpringCore/src/session01/test/TestHello.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import session01.bean.Hello;
 import session01.bean.Lotto;
 import session01.config.JavaSpringConfig;
+import session01.config.JavaSpringConfig2;
 
 //執行時請加入 VM 參數: --add-opens java.base/java.lang=ALL-UNNAMED
 //整體來說，這行命令的意思是：“在運行時，允許所有未命名的模組訪問 java.base 模組中的 java.lang 包”。
@@ -20,15 +21,16 @@ public class TestHello {
 		// 利用 Spring 來管理物件
 		// 使用 JavaSpringConfig(Java 配置) 來取得 bean 物件
 		// 1. 得到應用程式的配置環境
-		ApplicationContext ctx1 = new AnnotationConfigApplicationContext(JavaSpringConfig.class);
+		//ApplicationContext ctx1 = new AnnotationConfigApplicationContext(JavaSpringConfig.class);
+		ApplicationContext ctx2 = new AnnotationConfigApplicationContext(JavaSpringConfig2.class);
 		// 2. 取 bean
 		//Hello hello1 = ctx1.getBean("hello", Hello.class); // bean 的名字, bean 類型
 		//System.out.println(hello1.getToday());
 		
-		Lotto lotto1 = ctx1.getBean("lotto", Lotto.class);
+		Lotto lotto1 = ctx2.getBean("lotto", Lotto.class);
 		System.out.println(lotto1.getNumber());
 		
-		Lotto lotto2 = ctx1.getBean("lotto", Lotto.class);
+		Lotto lotto2 = ctx2.getBean("lotto", Lotto.class);
 		System.out.println(lotto2.getNumber());
 		
 	}

d5a3edebd4693a31c5b59a34fc96e1a0ac2119c0
aweit <aa4192696@gmail.com.tw>
2023-12-04 12:08:45 +0800

    java

--- SpringCore/src/session01/test/TestHello.java
+++ SpringCore/src/session01/test/TestHello.java
@@ -1,9 +1,13 @@
 package session01.test;
 
+import session01.bean.Hello;
+
 public class TestHello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 傳統: 自行管理物件
+		Hello hello = new Hello();
+		System.out.println(hello.getToday());
 
 	}
 

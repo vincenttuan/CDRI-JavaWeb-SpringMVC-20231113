065dfc18e80b32021a7c31b69b1fa64301a9b79b
aweit <aa4192696@gmail.com.tw>
2023-12-04 13:48:33 +0800

    java

---
+++ SpringCore/src/session01/config/JavaSpringConfig2.java
@@ -0,0 +1,10 @@
+package session01.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = "session01.bean")
+public class JavaSpringConfig2 {
+	
+}

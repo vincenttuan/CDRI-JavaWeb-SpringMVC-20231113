2f2b8808ff7b7bb981606ba7723df4533fbd6934
aweit <aa4192696@gmail.com.tw>
2023-12-04 12:14:16 +0800

    java

---
+++ SpringCore/src/session01/config/JavaSpringConfig.java
@@ -0,0 +1,20 @@
+package session01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import session01.bean.Hello;
+
+// JavaSpringConfig 是一個 Spring 配置檔, 來管理所定義的物件
+@Configuration
+public class JavaSpringConfig {
+	
+	@Bean(name = "hello") // bean 資源名稱
+	@Scope("singleton") // 物件範圍(生命週期). singleton(單一物件-共用), prototype(多物件-每次調用都新建一個) 
+	public Hello getHello() {
+		Hello hello = new Hello();
+		return hello;
+	}
+	
+}

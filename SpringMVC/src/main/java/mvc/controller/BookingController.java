b407a43c659be0d7a1ddf01b5e8b5344eee2574e
aweit <aa4192696@gmail.com.tw>
2023-12-05 11:53:35 +0800

    java

--- SpringMVC/src/main/java/mvc/controller/BookingController.java
+++ SpringMVC/src/main/java/mvc/controller/BookingController.java
@@ -6,7 +6,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * 會議室預訂系統
+ * 會議室預訂系統(Web API)
  * 假設您正在為一家公司開發一個會議室預訂系統。您需要實現一個控制器，該控制器可以處理會議室的預訂請求、取消請求以及查詢當前預訂狀態。
  * 
  * 功能要求：

0c78b3e20829eed88d71b67cc94139058338b0da
aweit <aa4192696@gmail.com.tw>
2023-12-06 16:14:36 +0800

    java

--- SpringCore/src/session02/proxy/Woman.java
+++ SpringCore/src/session02/proxy/Woman.java
@@ -4,8 +4,14 @@ public class Woman implements Person {
 
 	@Override
 	public void work() {
+		// 公用邏輯
+		System.out.println("出門戴口罩");
+		
 		// 業務邏輯
 		System.out.println("市場買菜");
+		
+		// 公用邏輯
+		System.out.println("回家洗手脫口罩");
 	}
 
 }

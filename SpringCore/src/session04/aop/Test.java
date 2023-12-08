7d319b50b7784f13011b61a5258fb7f169beed4c
aweit <aa4192696@gmail.com.tw>
2023-12-08 12:18:27 +0800

    java

--- SpringCore/src/session04/aop/Test.java
+++ SpringCore/src/session04/aop/Test.java
@@ -3,6 +3,9 @@ package session04.aop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//執行時要加入: JVM 啟動參數
+//--add-opens java.base/java.lang=ALL-UNNAMED
+//允許 Java 9 以上版本使用反射(java.lang.reflect)內部 API
 public class Test {
 
 	public static void main(String[] args) {

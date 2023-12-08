b0f2f589252d2a7a3494ca6accc5407d0919151d
aweit <aa4192696@gmail.com.tw>
2023-12-08 10:07:54 +0800

    java

--- SpringCore/src/session03/proxy_dyn/Test.java
+++ SpringCore/src/session03/proxy_dyn/Test.java
@@ -1,5 +1,9 @@
 package session03.proxy_dyn;
 
+import session02.proxy.Man;
+import session02.proxy.Person;
+import session02.proxy.Woman;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,7 +11,11 @@ public class Test {
 		Calc calc = (Calc)new DynProxy(new CalcImpl()).getProxy();
 		System.out.println(calc.add(20, 10));
 		System.out.println(calc.div(20, 10));
-
+		
+		Person woman = (Person)new DynProxy(new Woman()).getProxy();
+		woman.work();
+		Person man = (Person)new DynProxy(new Man()).getProxy();
+		man.work();
 	}
 
 }

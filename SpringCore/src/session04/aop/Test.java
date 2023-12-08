ccec618a2c1a7eb1f1e571eb147cdf4f19235760
aweit <aa4192696@gmail.com.tw>
2023-12-08 12:15:25 +0800

    java

---
+++ SpringCore/src/session04/aop/Test.java
@@ -0,0 +1,15 @@
+package session04.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-aop.xml");
+		
+		
+	}
+
+}

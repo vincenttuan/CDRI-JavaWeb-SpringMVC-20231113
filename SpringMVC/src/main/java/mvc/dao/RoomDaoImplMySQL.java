4306d01ff9aa2a68dbe31e8f8584129f42e4e677
aweit <aa4192696@gmail.com.tw>
2023-12-11 14:49:07 +0800

    java

--- SpringMVC/src/main/java/mvc/dao/RoomDaoImplMySQL.java
+++ SpringMVC/src/main/java/mvc/dao/RoomDaoImplMySQL.java
@@ -1,22 +1,38 @@
 package mvc.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import mvc.bean.Room;
 
 @Repository
 public class RoomDaoImplMySQL implements RoomDao {
-
+	
+	@Autowired
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
 	@Override
 	public List<Room> findAllRooms() {
-		return null;
+		String sql = "select roomId, roomName, roomSize from room order by roomId";
+		List<Room> rooms = namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Room.class));
+		return rooms;
 	}
 
 	@Override
 	public Room getRoom(Integer roomId) {
-		return null;
+		String sql = "select roomId, roomName, roomSize from room where roomId = :roomId";
+		
+		Map<String, Object> params = new HashMap<>();
+		params.put("roomId", roomId);
+		
+		Room room = namedParameterJdbcTemplate.queryForObject(sql, params, new BeanPropertyRowMapper<>(Room.class));
+		return room;
 	}
 
 }

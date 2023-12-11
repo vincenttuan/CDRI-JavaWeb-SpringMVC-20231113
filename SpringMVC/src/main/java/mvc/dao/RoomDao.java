ee7e0ab63f7dcb6873dc4dfa4289ec8acdddd62a
aweit <aa4192696@gmail.com.tw>
2023-12-11 14:23:29 +0800

    java

--- SpringMVC/src/main/java/mvc/dao/RoomDao.java
+++ SpringMVC/src/main/java/mvc/dao/RoomDao.java
@@ -1,13 +1,12 @@
 package mvc.dao;
 
 import java.util.List;
-import java.util.Optional;
 
 import mvc.bean.Room;
 
 public interface RoomDao {
 	
 	List<Room> findAllRooms();
-	Optional<Room> getRoom(Integer roomId);
+	Room getRoom(Integer roomId);
 	
 }

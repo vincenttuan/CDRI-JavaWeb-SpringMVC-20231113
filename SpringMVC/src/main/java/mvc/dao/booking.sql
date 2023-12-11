072a87ae2a84497842373814aad1921b47749d86
aweit <aa4192696@gmail.com.tw>
2023-12-11 11:00:30 +0800

    java

--- SpringMVC/src/main/java/mvc/dao/booking.sql
+++ SpringMVC/src/main/java/mvc/dao/booking.sql
@@ -17,3 +17,17 @@ create table if not exists BookingRoom(
     foreign key (roomId) references Room(roomId),
     constraint unique_roomId_and_bookingDate unique(roomId, bookingDate)
 );
+
+-- 建立範例資料
+insert into Room(roomId, roomName, roomSize) values(101, '101(S)', 10);
+insert into Room(roomId, roomName, roomSize) values(102, '102(M)', 25);
+insert into Room(roomId, roomName, roomSize) values(203, '203(L)', 50);
+insert into Room(roomId, roomName, roomSize) values(404, '404(2L)', 100);
+
+insert into BookingRoom(roomId, username, bookingDate) values(101, 'Tom', '2023-12-04');
+insert into BookingRoom(roomId, username, bookingDate) values(102, 'Mary', '2023-12-05');
+insert into BookingRoom(roomId, username, bookingDate) values(102, 'Rose', '2023-12-06');
+
+
+
+

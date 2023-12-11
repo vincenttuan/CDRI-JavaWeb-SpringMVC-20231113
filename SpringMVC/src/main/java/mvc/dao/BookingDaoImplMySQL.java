7cf19cdb32c7b88a2647294ecf1f19281878b5bd
aweit <aa4192696@gmail.com.tw>
2023-12-11 12:11:18 +0800

    java

--- SpringMVC/src/main/java/mvc/dao/BookingDaoImplMySQL.java
+++ SpringMVC/src/main/java/mvc/dao/BookingDaoImplMySQL.java
@@ -1,10 +1,14 @@
 package mvc.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import mvc.bean.BookingRoom;
@@ -19,25 +23,52 @@ public class BookingDaoImplMySQL implements BookingDao {
 	@Override
 	public int addBookRoom(BookingRoom bookingRoom) {
 		String sql = "insert into BookingRoom(roomId, username, bookingDate) values(?, ?, ?)";
-		return jdbcTemplate.update(sql, bookingRoom.getRoomId(), bookingRoom.getUsername(), bookingRoom.getBookingDate());
+		int rowcount = jdbcTemplate.update(sql, bookingRoom.getRoomId(), bookingRoom.getUsername(), bookingRoom.getBookingDate());
+		return rowcount;
 	}
 
 	@Override
 	public int cancelBooking(Integer bookingId) {
 		String sql = "delete from BookingRoom where bookingId = ?";
-		return jdbcTemplate.update(sql, bookingId);
+		int rowcount = jdbcTemplate.update(sql, bookingId);
+		return rowcount;
 	}
 
 	@Override
 	public List<BookingRoom> findAllBookingRooms() {
 		String sql = "select bookingId, roomId, username, bookingDate, createDate from BookingRoom order by bookingId";
-		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(BookingRoom.class));
+		// 定義對應邏輯/規則
+		RowMapper<BookingRoom> mapper = new RowMapper<BookingRoom>() {
+			@Override
+			public BookingRoom mapRow(ResultSet rs, int rowNum) throws SQLException {
+				// 取出資料列的每一個欄位資訊
+				Integer bookingId = rs.getInt("bookingId");
+				Integer roomId = rs.getInt("roomId");
+				String username = rs.getString("username");
+				String bookingDate = rs.getString("bookingDate");
+				Timestamp createDate = rs.getTimestamp("createDate");
+				// 將上述欄位資訊注入到 bookingRoom 物件中
+				BookingRoom bookingRoom = new BookingRoom();
+				bookingRoom.setBookingId(bookingId);
+				bookingRoom.setRoomId(roomId);
+				bookingRoom.setUsername(username);
+				bookingRoom.setBookingDate(bookingDate);
+				bookingRoom.setCreateDate(createDate);
+				return bookingRoom;
+			}
+		};
+		
+		List<BookingRoom> bookingRooms = jdbcTemplate.query(sql, mapper);
+		return bookingRooms;
+		
+		//return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(BookingRoom.class));
 	}
 
 	@Override
 	public int updateBookingUsername(Integer bookingId, String newUsername) {
 		String sql = "update BookingRoom set username = ? where bookingId = ?";
-		return jdbcTemplate.update(sql, newUsername, bookingId);
+		int rowcount = jdbcTemplate.update(sql, newUsername, bookingId);
+		return rowcount;
 	}
 	
 }

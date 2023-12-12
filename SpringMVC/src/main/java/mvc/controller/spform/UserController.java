3c1d214b970eafe8d849b3baaa270e181641b762
aweit <aa4192696@gmail.com.tw>
2023-12-12 11:14:15 +0800

    java

--- SpringMVC/src/main/java/mvc/controller/spform/UserController.java
+++ SpringMVC/src/main/java/mvc/controller/spform/UserController.java
@@ -1,21 +1,29 @@
 package mvc.controller.spform;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mvc.bean.spform.EducationData;
 import mvc.bean.spform.User;
+import mvc.dao.spform.DataDao;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	@Autowired
+	private DataDao dataDao;
+	
 	@GetMapping("/")
-	public String index(@ModelAttribute User user) {
-		user.setName("John");
-		user.setAge(18);
+	public String index(@ModelAttribute User user, Model model) {
+		List<EducationData> educations = dataDao.findAllEducationDatas();
+		model.addAttribute("educations", educations); // 將教育程度資料傳給 jsp
 		return "spform/user";
 	}
 	

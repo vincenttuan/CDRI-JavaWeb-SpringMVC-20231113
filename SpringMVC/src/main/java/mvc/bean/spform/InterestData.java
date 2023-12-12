6af717ea09f767cdbd8ea23b82c6ae4dd36703cd
aweit <aa4192696@gmail.com.tw>
2023-12-12 10:14:13 +0800

    java

--- SpringMVC/src/main/java/mvc/bean/spform/InterestData.java
+++ SpringMVC/src/main/java/mvc/bean/spform/InterestData.java
@@ -1,5 +1,36 @@
 package mvc.bean.spform;
 
 public class InterestData {
+	private Integer id;
+	private String name;
+	
+	public InterestData( ) {
+		
+	}
+	
+	public InterestData(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "InterestData [id=" + id + ", name=" + name + "]";
+	}
 }
--- SpringMVC/src/main/java/mvc/bean/spform/SexData.java
+++ SpringMVC/src/main/java/mvc/bean/spform/SexData.java
@@ -1,5 +1,38 @@
 package mvc.bean.spform;
 
 public class SexData {
+	private Integer id;
+	private String name;
+	
+	public SexData( ) {
+		
+	}
+	
+	public SexData(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "SexData [id=" + id + ", name=" + name + "]";
+	}
+	
+	
 }

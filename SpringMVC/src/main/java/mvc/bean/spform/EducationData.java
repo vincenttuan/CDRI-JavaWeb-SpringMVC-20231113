362f4ae46dcfaa3bc0164bc65038e342352a782c
aweit <aa4192696@gmail.com.tw>
2023-12-12 10:13:03 +0800

    java

--- SpringMVC/src/main/java/mvc/bean/spform/EducationData.java
+++ SpringMVC/src/main/java/mvc/bean/spform/EducationData.java
@@ -1,5 +1,38 @@
 package mvc.bean.spform;
 
+// 教育程度資料
 public class EducationData {
+	private Integer id;
+	private String name;
+	
+	public EducationData( ) {
+		
+	}
+	
+	public EducationData(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "EducationData [id=" + id + ", name=" + name + "]";
+	}
+	
 }

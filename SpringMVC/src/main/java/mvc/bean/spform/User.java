d9b9669671860c62e06d8faca1523d0a342cb144
aweit <aa4192696@gmail.com.tw>
2023-12-12 10:24:49 +0800

    java

--- SpringMVC/src/main/java/mvc/bean/spform/User.java
+++ SpringMVC/src/main/java/mvc/bean/spform/User.java
@@ -28,4 +28,81 @@ public class User {
 	private Integer interestId; // 興趣Id
 	private List<InterestData> interests; // 興趣
 	
+	// ---------------------------------------------------------
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public String getResume() {
+		return resume;
+	}
+	public void setResume(String resume) {
+		this.resume = resume;
+	}
+	public Integer getEducationId() {
+		return educationId;
+	}
+	public void setEducationId(Integer educationId) {
+		this.educationId = educationId;
+	}
+	public EducationData getEducation() {
+		return education;
+	}
+	public void setEducation(EducationData education) {
+		this.education = education;
+	}
+	public Integer getSexId() {
+		return sexId;
+	}
+	public void setSexId(Integer sexId) {
+		this.sexId = sexId;
+	}
+	public SexData getSex() {
+		return sex;
+	}
+	public void setSex(SexData sex) {
+		this.sex = sex;
+	}
+	public Integer getInterestId() {
+		return interestId;
+	}
+	public void setInterestId(Integer interestId) {
+		this.interestId = interestId;
+	}
+	public List<InterestData> getInterests() {
+		return interests;
+	}
+	public void setInterests(List<InterestData> interests) {
+		this.interests = interests;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", birth=" + birth + ", resume=" + resume
+				+ ", educationId=" + educationId + ", education=" + education + ", sexId=" + sexId + ", sex=" + sex
+				+ ", interestId=" + interestId + ", interests=" + interests + "]";
+	}
+	
+	
+	
 }

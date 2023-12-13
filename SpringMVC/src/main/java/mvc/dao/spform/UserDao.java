763076ec4f41158e622ada1f67a186fcfe6e2189
aweit <aa4192696@gmail.com.tw>
2023-12-13 10:06:12 +0800

    java

---
+++ SpringMVC/src/main/java/mvc/dao/spform/UserDao.java
@@ -0,0 +1,16 @@
+package mvc.dao.spform;
+
+import java.util.List;
+import java.util.Optional;
+
+import mvc.bean.spform.User;
+
+public interface UserDao {
+	
+	int addUser(User user);
+	int updateUserById(Integer id, User user);
+	int deleteUserById(Integer id);
+	Optional<User> getUserById(Integer id);
+	List<User> findAllUsers();
+	
+}

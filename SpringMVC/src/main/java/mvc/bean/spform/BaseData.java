408291326db5cfafd9e6ab5e921c3f569b0b363c
aweit <aa4192696@gmail.com.tw>
2023-12-15 14:05:08 +0800

    java

---
+++ SpringMVC/src/main/java/mvc/bean/spform/BaseData.java
@@ -0,0 +1,34 @@
+package mvc.bean.spform;
+
+public abstract class BaseData {
+	protected Integer id;
+	protected String name;
+	
+	public BaseData() {
+	}
+	
+	public BaseData(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "BaseData [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}

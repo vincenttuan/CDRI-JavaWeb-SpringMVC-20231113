65388cfe1e72ddb3f616f960d3d6eee85566f228
aweit <aa4192696@gmail.com.tw>
2023-12-15 14:45:52 +0800

    java

---
+++ SpringMVC/src/main/java/mvc/dao/spform/DataDaoImplMySQL.java
@@ -0,0 +1,52 @@
+package mvc.dao.spform;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import mvc.bean.spform.EducationData;
+import mvc.bean.spform.InterestData;
+import mvc.bean.spform.SexData;
+
+@Repository
+public class DataDaoImplMySQL implements DataDao {
+
+	@Override
+	public List<EducationData> findAllEducationDatas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<EducationData> getEducationDataById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<SexData> findAllSexDatas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<SexData> getSexDataById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public List<InterestData> finAllInterestDatas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<InterestData> getInterestDataById(Integer id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+	
+}

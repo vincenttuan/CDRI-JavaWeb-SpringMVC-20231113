002d2641412f142a42d02935ae9faa99f2d853e7
aweit <aa4192696@gmail.com.tw>
2023-12-15 14:28:46 +0800

    java

---
+++ SpringMVC/src/main/java/mvc/dao/spform/dao.sql
@@ -0,0 +1,19 @@
+create table if not exists BaseData(
+	groupName varchar(50),
+    itemId int,
+    itemName varchar(50)
+);
+
+insert into BaseData(groupName, itemId, itemName) values('Education', 1, '小學');
+insert into BaseData(groupName, itemId, itemName) values('Education', 2, '國中');
+insert into BaseData(groupName, itemId, itemName) values('Education', 3, '高中');
+insert into BaseData(groupName, itemId, itemName) values('Education', 4, '大學');
+insert into BaseData(groupName, itemId, itemName) values('Education', 5, '研究所');
+insert into BaseData(groupName, itemId, itemName) values('Sex', 1, '男');
+insert into BaseData(groupName, itemId, itemName) values('Sex', 2, '女');
+insert into BaseData(groupName, itemId, itemName) values('Interest', 1, '爬山 🏕');
+insert into BaseData(groupName, itemId, itemName) values('Interest', 2, '音樂 🎶');
+insert into BaseData(groupName, itemId, itemName) values('Interest', 3, '看書');
+insert into BaseData(groupName, itemId, itemName) values('Interest', 4, '刺繡');
+insert into BaseData(groupName, itemId, itemName) values('Interest', 5, '國畫');
+insert into BaseData(groupName, itemId, itemName) values('Interest', 6, 'FPV ✈');

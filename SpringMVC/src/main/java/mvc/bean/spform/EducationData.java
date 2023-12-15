c97f53beec1fe6c9e10e7df229b6cb65e5038ed9
aweit <aa4192696@gmail.com.tw>
2023-12-15 14:14:13 +0800

    java

--- SpringMVC/src/main/java/mvc/bean/spform/EducationData.java
+++ SpringMVC/src/main/java/mvc/bean/spform/EducationData.java
@@ -4,9 +4,13 @@ import com.google.gson.Gson;
 
 // 教育程度資料
 public class EducationData extends BaseData {
-
+	
 	public EducationData(Integer id, String name) {
 		super(id, name);
+	}
+
+	public String getDisplay() {
+		return name + "(" + id + ")";
 	}
 	
 	

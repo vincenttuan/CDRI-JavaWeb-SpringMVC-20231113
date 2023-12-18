8664792aca7ab17337041e4488a8a79b053074a9
aweit <aa4192696@gmail.com.tw>
2023-12-18 15:26:24 +0800

    java

---
+++ SpringCore/src/session05/tx/service/BookOneService.java
@@ -0,0 +1,6 @@
+package session05.tx.service;
+
+public interface BookOneService {
+	void buyOne(String username, Integer bookId); // 購書服務
+	//void refundOne(String username, Integer bookId); // 退書服務
+}

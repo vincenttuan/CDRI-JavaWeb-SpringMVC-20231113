d2067963eeb33aa89b2cc48f4ff5988cc6c3b0e8
aweit <aa4192696@gmail.com.tw>
2023-12-18 14:28:35 +0800

    java

---
+++ SpringCore/src/pattern/decorator/Test.java
@@ -0,0 +1,20 @@
+package pattern.decorator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Food food = new Bread();
+		food = new Ham(food);
+		food = new Ham(food);
+		food = new Ham(food);
+		food = new Tuna(food);
+		food = new Lettue(food);
+		
+		// 結帳
+		System.out.printf("結帳:\n");
+		System.out.printf("商品: %s\n", food.getName());
+		System.out.printf("總價: %d\n", food.getPrice());
+
+	}
+
+}

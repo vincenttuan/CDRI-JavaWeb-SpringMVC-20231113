bc54b9247c06555b5d64af95a7be7c9269e1b30f
aweit <aa4192696@gmail.com.tw>
2023-12-18 16:04:04 +0800

    java

---
+++ SpringCore/src/session05/tx/test/BuyOneBookTest.java
@@ -0,0 +1,18 @@
+package session05.tx.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import session05.tx.controller.BookController;
+
+public class BuyOneBookTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-tx-config.xml");
+		BookController bookController = ctx.getBean(BookController.class);
+		
+		bookController.buyOneBook("John", 1);
+
+	}
+
+}

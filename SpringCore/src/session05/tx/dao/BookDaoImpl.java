839feb71e3ff759f18cad489a559b94cc0b68b56
aweit <aa4192696@gmail.com.tw>
2023-12-18 15:14:59 +0800

    java

--- SpringCore/src/session05/tx/dao/BookDaoImpl.java
+++ SpringCore/src/session05/tx/dao/BookDaoImpl.java
@@ -11,31 +11,31 @@ public class BookDaoImpl implements BookDao {
 	private JdbcTemplate jdbcTemplate;
 	
 	@Override
-	public Integer getBookPrice(Integer bookId) {
+	public Integer getBookPrice(Integer bookId) { // 取得書本價格
 		String sql = "select book_price from book where book_id = ?";
 		return jdbcTemplate.queryForObject(sql, Integer.class, bookId);
 	}
 
 	@Override
-	public Integer getBookStock(Integer bookId) {
+	public Integer getBookStock(Integer bookId) { // 取得書本庫存
+		String sql = "select book_amount from stock where book_id = ?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, bookId);
+	}
+
+	@Override
+	public Integer getWalletBalance(String username) { // 取得客戶目前餘額
+		String sql = "select balance from wallet where username = ?";
+		return jdbcTemplate.queryForObject(sql, Integer.class, username);
+	}
+
+	@Override
+	public Integer reduceBookStock(Integer bookId, Integer amountToReduce) { // 更新書本庫存(減量)
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Integer getWalletBalance(String username) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer reduceBookStock(Integer bookId, Integer amountToReduce) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer reduceWalletBalance(String username, Integer bookPrice) {
+	public Integer reduceWalletBalance(String username, Integer bookPrice) { // 更新錢包餘額(減量)
 		// TODO Auto-generated method stub
 		return null;
 	}

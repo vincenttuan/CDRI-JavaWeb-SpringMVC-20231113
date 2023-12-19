f99b04e937f5d0195a51d9b28f0b611ae097e0a8
aweit <aa4192696@gmail.com.tw>
2023-12-19 11:05:36 +0800

    java

--- SpringCore/src/session05/tx/service/BookOneService.java
+++ SpringCore/src/session05/tx/service/BookOneService.java
@@ -1,6 +1,9 @@
 package session05.tx.service;
 
+import session05.tx.exception.InsufficientAmount;
+import session05.tx.exception.InsufficientStock;
+
 public interface BookOneService {
-	void buyOne(String username, Integer bookId); // 購書服務
+	void buyOne(String username, Integer bookId) throws InsufficientAmount, InsufficientStock; // 購書服務
 	//void refundOne(String username, Integer bookId); // 退書服務
 }

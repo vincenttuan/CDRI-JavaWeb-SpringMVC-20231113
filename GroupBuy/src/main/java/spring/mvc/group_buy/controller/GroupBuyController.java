b942d1d636db8c0ffea10f6438a8861ff71e6972
aweit <aa4192696@gmail.com.tw>
2023-12-20 11:17:17 +0800

    java

--- GroupBuy/src/main/java/spring/mvc/group_buy/controller/GroupBuyController.java
+++ GroupBuy/src/main/java/spring/mvc/group_buy/controller/GroupBuyController.java
@@ -1,17 +1,52 @@
 package spring.mvc.group_buy.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import spring.mvc.group_buy.model.dao.GroupBuyDao;
+import spring.mvc.group_buy.model.entity.User;
 
 @Controller
 @RequestMapping("/group_buy")
 public class GroupBuyController {
 	
+	@Autowired
+	private GroupBuyDao dao;
+	
 	// 登入首頁
 	@GetMapping(value = {"/login", "/", "/login/"})
 	public String loginPage() {
 		return "group_buy/login";
+	}
+	
+	// 前台登入處理
+	@PostMapping("/login")
+	@ResponseBody
+	public String login(@RequestParam("username") String username, 
+						 @RequestParam("password") String password, 
+						HttpSession session) {
+		// 根據 username 查找 user 物件
+		Optional<User> userOpt = dao.findUserByUsername(username);
+		if(userOpt.isPresent()) {
+			User user = userOpt.get();
+			// 比對 password
+			if(user.getPassword().equals(password)) {
+				return "login: ok";
+			} else {
+				return "login: password fail";
+			}
+		} else {
+			return "login: username not found";
+		}
 	}
 	
 }

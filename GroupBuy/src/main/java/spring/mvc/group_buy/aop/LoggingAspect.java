8b064959af8edd3758ff8e95c07809116c988e03
aweit <aa4192696@gmail.com.tw>
2023-12-20 10:00:27 +0800

    java

--- GroupBuy/src/main/java/spring/mvc/group_buy/aop/LoggingAspect.java
+++ GroupBuy/src/main/java/spring/mvc/group_buy/aop/LoggingAspect.java
@@ -1,5 +1,19 @@
 package spring.mvc.group_buy.aop;
 
-public class LoggingAspect {
+import java.util.Arrays;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+	@Before("execution(* spring.mvc.group_buy.controller.*.*(..))")
+	public void before(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName();
+		Object[] args = joinPoint.getArgs();
+		System.out.printf("調用方法: %s 參數: %s%n", methodName, Arrays.toString(args));
+	}
 }

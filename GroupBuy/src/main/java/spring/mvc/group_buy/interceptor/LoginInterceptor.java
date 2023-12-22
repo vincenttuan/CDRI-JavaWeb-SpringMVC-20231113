744e2e120d2f33e98d307326dfcfd95b367c2213
aweit <aa4192696@gmail.com.tw>
2023-12-22 16:38:20 +0800

    java

--- GroupBuy/src/main/java/spring/mvc/group_buy/interceptor/LoginInterceptor.java
+++ GroupBuy/src/main/java/spring/mvc/group_buy/interceptor/LoginInterceptor.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import spring.mvc.group_buy.model.entity.User;
+
 public class LoginInterceptor implements HandlerInterceptor {
 
 	@Override
@@ -15,6 +17,18 @@ public class LoginInterceptor implements HandlerInterceptor {
 		HttpSession session = request.getSession();
 		// 檢查 session 中是否有 user 的物件資料(意味著用戶已經登入)
 		if(session.getAttribute("user") != null) {
+			User user = (User)session.getAttribute("user");
+			// 路徑的權限檢查
+			// "/group_buy/backend", user level = 2 才可以進入
+			System.out.println("RequestURI = " + request.getRequestURI());
+			if(request.getRequestURI().contains("/group_buy/backend")) { // 後台
+				if(user.getLevel() == 2) {
+					return true; // 放行
+				} else {
+					response.sendRedirect(request.getServletContext().getContextPath() + "/mvc/group_buy/login");
+					return false; // 不放行
+				}
+			} 
 			return true; // 放行
 		}
 		// 未登入, 導入到登入頁面

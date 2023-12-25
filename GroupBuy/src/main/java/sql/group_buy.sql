618d0e535e6842e0a0a6b3504b8d31a5dbee13de
aweit <aa4192696@gmail.com.tw>
2023-12-25 09:31:14 +0800

    java

--- GroupBuy/src/main/java/sql/group_buy.sql
+++ GroupBuy/src/main/java/sql/group_buy.sql
@@ -49,10 +49,37 @@ ps: cartItems 一對多關聯
 資料庫的建立: CREATE SCHEMA `group_buy` DEFAULT CHARACTER SET utf8mb4 ;
 
  * */
+-- 授權資料表
+drop table if exists service;
+drop table if exists level;
+drop table if exists level_ref_service;
+
 drop table if exists cartitem;
 drop table if exists cart;
 drop table if exists user;
 drop table if exists product;
+
+-- 建立 Level
+create table if not exists level(
+    levelId int primary key,
+    levelName varchar(50)
+);
+
+-- 建立 Service table
+create table if not exists service(
+    serviceId int primary key,
+    serviceName varchar(50),
+    serviceUrl varchar(50)
+);
+
+-- 建立 Service 與 Level 之間的關聯表 table
+create table if not exists level_ref_service(
+    levelId int not null,
+    serviceId int not null,
+    foreign key (levelId) references level(levelId),
+    foreign key (serviceId) references service(serviceId),
+    constraint unique_sid_and_aid UNIQUE(levelId, serviceId)
+);
 
 -- 建立 Product
 create table if not exists product(
@@ -68,7 +95,7 @@ alter table product auto_increment = 501;
 -- 建立 User
 create table if not exists user(
 	userId int auto_increment primary key,
-    username varchar(50) unique not null,
+    username varchar(50) not null,
     password varchar(50) not null,
     level int
 );
@@ -93,8 +120,7 @@ create table if not exists cartitem(
     productId int not null,
     quantity int default 0,
     foreign key (cartId) references cart(cartId),
-    foreign key (productId) references product(productId),
-    constraint unique_cartId_and_productId unique(cartId, productId)
+    foreign key (productId) references product(productId)
 );
 -- 設置 AUTO_INCREMENT = 1
 alter table cartitem auto_increment = 1;
@@ -128,14 +154,4 @@ INSERT INTO cartitem (itemId, cartId, productId, quantity) VALUES
 (5, 203, 504, 20),
 (6, 205, 505, 15);
 
--- 每個使用者的總消費金額
--- 建立 View
-create view UserTotalAmountView as
-select u.userId, u.username, coalesce(SUM(p.price * ci.quantity), 0) as total
-from user u
-left join cart c on u.userId = c.userId
-left join cartitem ci on c.cartId = ci.cartId
-left join product p on ci.productId = p.productId
-where c.isCheckout = true
-group by u.userId, u.username
 

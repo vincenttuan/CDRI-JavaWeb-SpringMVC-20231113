ce89a83533278c9ead02b58d5d7078030b53e589
aweit <aa4192696@gmail.com.tw>
2023-12-26 10:30:22 +0800

    java

--- JavaCryptography/src/security/hash/SalaryHashGenerator.java
+++ JavaCryptography/src/security/hash/SalaryHashGenerator.java
@@ -6,8 +6,7 @@ import security.KeyUtil;
 public class SalaryHashGenerator {
 	public static void main(String[] args) {
 		String filepath = "src/security/hash/my_salary.txt";
-		String filehash = KeyUtil.generateFileHash(filepath);
-		
+		String filehash = KeyUtil.generateFileHash("src/security/hash/my_salary.txt");
 		System.out.println("my_salary.txt hash:" + filehash);
 	}
 }

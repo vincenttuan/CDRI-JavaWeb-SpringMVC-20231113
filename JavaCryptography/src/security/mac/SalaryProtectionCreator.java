ab9d4579af7143d118ed45fd30bf62bd3150c0b5
aweit <aa4192696@gmail.com.tw>
2023-12-27 10:06:50 +0800

    java

--- JavaCryptography/src/security/mac/SalaryProtectionCreator.java
+++ JavaCryptography/src/security/mac/SalaryProtectionCreator.java
@@ -5,9 +5,12 @@ package security.mac;
  * 在一家大型企業，HR部門每月都會發送電子薪資明細給員工。
  * 為了確保薪資明細的安全性，HR部門決定對薪資明細檔案進行雜湊和 MAC 保護。
  * 雜湊保護確保薪資明細的完整性，而MAC則確保薪資明細確實來自HR部門，並未被其他部門或外部攻擊者更改。
- * 
+ * 薪資檔案位置   : src/security/mac/my_salary.txt
+ * macKey檔案位置: src/security/mac/macKey.key
  * */
 
 public class SalaryProtectionCreator {
-
+	public static void main(String[] args) {
+		
+	}
 }

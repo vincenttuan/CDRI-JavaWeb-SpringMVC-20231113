5a673d0577901911d15e04ae44a4330c5b571d4a
aweit <aa4192696@gmail.com.tw>
2023-12-27 11:39:29 +0800

    java

--- JavaCryptography/src/security/sign/DigitalSignatureVerify.java
+++ JavaCryptography/src/security/sign/DigitalSignatureVerify.java
@@ -1,5 +1,13 @@
 package security.sign;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import security.KeyUtil;
+
 /*
  * 情境說明：
  * 延續「DigitalSignatureCreator」的情境，小王的合作夥伴，小李，收到了小王用數位簽章簽署的「my_contract.txt」文件。
@@ -10,7 +18,7 @@ package security.sign;
  * 如果驗證成功，這意味著文件在傳輸過程中沒有被修改，且確實是由小王簽署的。
  */
 public class DigitalSignatureVerify {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// 小王
 		// 合約檔位置
 		String contractPath = "src/security/sign/my_contract.txt";
@@ -19,6 +27,34 @@ public class DigitalSignatureVerify {
 		// 私鑰檔位置
 		String privateKeyPath = "src/security/sign/privateKey.txt";
 		// 數位簽章檔位置
-		String signaturePath = "src/security/sign/signature.sig"; 
+		String signaturePath = "src/security/sign/signature.sig";
+		
+		// 公鑰與私鑰
+		PublicKey publicKey;
+		PrivateKey privateKey;
+		
+		// 取得公私鑰/建立私公鑰
+		if(Files.exists(Paths.get(publicKeyPath)) && Files.exists(Paths.get(privateKeyPath))) {
+			// 取得公私鑰
+			publicKey = KeyUtil.getPublicKeyFromFile("RSA", publicKeyPath);
+			privateKey = KeyUtil.getPrivateKeyFromFile("RSA", privateKeyPath);
+		} else {
+			// 建立公私鑰
+			KeyPair keyPair = KeyUtil.generateRSAKeyPair();
+			publicKey = keyPair.getPublic();
+			privateKey = keyPair.getPrivate();
+			// 保存公私鑰檔案
+			KeyUtil.saveKeyToFile(publicKey, publicKeyPath);
+			KeyUtil.saveKeyToFile(privateKey, privateKeyPath);
+		}
+		
+		// 數位簽章: 將私鑰(私人印章) 蓋在合約(my_contract.txt)上
+		byte[] digitalSignature = KeyUtil.signWithPrivateKey(privateKey, contractPath);
+		
+		// 保存數位簽章
+		KeyUtil.saveSignatureToFile(digitalSignature,  signaturePath);
+		
+		
+		
 	}
 }

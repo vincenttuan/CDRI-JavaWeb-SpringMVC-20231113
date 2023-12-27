e3cf5a687d69be8c65b6cac55450dccd8bde3489
aweit <aa4192696@gmail.com.tw>
2023-12-27 11:42:52 +0800

    java

--- JavaCryptography/src/security/sign/DigitalSignatureVerify.java
+++ JavaCryptography/src/security/sign/DigitalSignatureVerify.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Base64;
 
 import security.KeyUtil;
 
@@ -23,9 +24,9 @@ public class DigitalSignatureVerify {
 		// 合約檔位置
 		String contractPath = "src/security/sign/my_contract.txt";
 		// 公鑰檔位置
-		String publicKeyPath = "src/security/sign/publicKey.txt";
+		String publicKeyPath = "src/security/sign/publicKey.key";
 		// 私鑰檔位置
-		String privateKeyPath = "src/security/sign/privateKey.txt";
+		String privateKeyPath = "src/security/sign/privateKey.key";
 		// 數位簽章檔位置
 		String signaturePath = "src/security/sign/signature.sig";
 		
@@ -54,7 +55,9 @@ public class DigitalSignatureVerify {
 		// 保存數位簽章
 		KeyUtil.saveSignatureToFile(digitalSignature,  signaturePath);
 		
-		
+		System.out.println("小王的公鑰(驗證數位簽章用):" + Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+		System.out.println("小王的數位簽章:" + Base64.getEncoder().encodeToString(digitalSignature));
+		System.out.println("小王的數位簽章位置:" + signaturePath);
 		
 	}
 }
---
+++ JavaCryptography/src/security/sign/signature.sig
@@ -0,0 +1,3 @@
+¹ë‚92ÎžS*ŠJP[¡ÏÑL¯-ÚÁÏ×çô_~ø5<6Ó‡Ö*š‚ÚPvÃûµ¨PÆI:üì|ßÑññ.2çÆÛˆÆw+5à	ôÂJ§å‘õ¶¿lþ9Á©¤ãÜÚ¸M£O
+FÚu+ñýw[‚-H¾y5ƒø¨~2ÿ›+G¤úpB¡Ï~Hnrïþ®ùq´Žâ›"‰49oHX
+fç•<~29Ú2Ð¹©—·aœªì82‹DdËÜ)áÄI¥ÝTI[O¾rºUìæØŸ1³Æ«aÌÁohåG•a‡¬ÃéNy¡0>C*$½n+Ôü€ÖX›íDŽ

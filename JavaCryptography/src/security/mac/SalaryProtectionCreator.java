25de95b65654560642fab7f487439b639b56db44
aweit <aa4192696@gmail.com.tw>
2023-12-27 10:21:55 +0800

    java

--- JavaCryptography/src/security/mac/SalaryProtectionCreator.java
+++ JavaCryptography/src/security/mac/SalaryProtectionCreator.java
@@ -1,5 +1,9 @@
 package security.mac;
 
+import javax.crypto.SecretKey;
+
+import security.KeyUtil;
+
 /*
  * 情境描述：
  * 在一家大型企業，HR部門每月都會發送電子薪資明細給員工。
@@ -8,9 +12,34 @@ package security.mac;
  * 薪資檔案位置   : src/security/mac/my_salary.txt
  * macKey檔案位置: src/security/mac/macKey.key
  * */
-
 public class SalaryProtectionCreator {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		// HR:
+		String filePath = "src/security/mac/my_salary.txt";
+		String keyPath = "src/security/mac/macKey.key";
+		
+		System.out.println("HR 生成:");
+		System.out.println("真對薪資檔案位置:src/security/mac/my_salary.txt 生成 macValue(Hex)");
+		
+		// 針對 filePath 檔案內容生成雜湊
+		String fileHash = KeyUtil.generateFileHash(filePath);
+		System.out.println("File Hash(my_salary.txt):" + fileHash);
+		
+		// 生成 MAC Key
+		SecretKey macKey = KeyUtil.generateKeyForHmac();
+		// 儲存 MAC Key
+		KeyUtil.saveSecretKeyToFile(macKey, keyPath);
+		
+		// 得到 macValue(Hex)
+		String macValueHex = KeyUtil.generateMac("HmacSHA256", macKey, filePath);
+		System.out.println("macValue(Hex):" + macValueHex);
+		
+		System.out.println("-------------------------------------------------------");
+		// 員工會得到
+		System.out.println("員工會得到:");
+		System.out.println("薪資檔案位置:" + filePath);
+		System.out.println("薪資macValue(Hex):" + macValueHex);
+		
 		
 	}
 }

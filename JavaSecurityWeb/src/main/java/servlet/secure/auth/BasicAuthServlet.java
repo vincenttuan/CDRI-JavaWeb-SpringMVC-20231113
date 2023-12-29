c0e60d8b4b2cb83d3980fe546abd90717ae6967c
aweit <aa4192696@gmail.com.tw>
2023-12-29 10:49:10 +0800

    java

--- JavaSecurityWeb/src/main/java/servlet/secure/auth/BasicAuthServlet.java
+++ JavaSecurityWeb/src/main/java/servlet/secure/auth/BasicAuthServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import servlet.util.WebKeyUtil;
+
 /*
  * BasicAuthServlet 是一個實現了 HTTP Basic Authentication 的 Servlet。
  * 由於用戶名和密碼在 HTTP 請求中以 Base64 編碼的形式發送（而非加密形式），
@@ -22,7 +24,21 @@ public class BasicAuthServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().println("Welcome to the protected page !");
+		// 檢查 client 端的 http header 中的 Authorization 標頭資訊
+		String authHeader = req.getHeader("Authorization");
+		
+		if(authHeader != null && WebKeyUtil.isValidBasicAuth(authHeader)) { // 進行標頭認證檢查
+			// 認證成功
+			resp.getWriter().println("Welcome to the protected page !");
+			return;
+		}
+		
+		// 認證失敗
+		final String REALM = "Restricted Area"; // 定義領域
+		// 要求客戶端提供認證訊息
+		resp.setHeader("WWW-Authenticate", WebKeyUtil.generateBasicChallenge(REALM));
+		// 發送 401 未經授權的回應
+		resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 	
 }

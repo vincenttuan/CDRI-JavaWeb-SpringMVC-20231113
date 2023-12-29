9d97fd2e04d035cff7785ca00e87e1029650620e
aweit <aa4192696@gmail.com.tw>
2023-12-29 10:41:23 +0800

    java

--- JavaSecurityWeb/src/main/java/servlet/secure/auth/BasicAuthServlet.java
+++ JavaSecurityWeb/src/main/java/servlet/secure/auth/BasicAuthServlet.java
@@ -22,7 +22,7 @@ public class BasicAuthServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		resp.getWriter().println("Welcome to the protected page !");
 	}
 	
 }

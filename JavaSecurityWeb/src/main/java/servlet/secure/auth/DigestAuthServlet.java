2cba3386a26da36381e5f6b5f70208b13623cb92
aweit <aa4192696@gmail.com.tw>
2023-12-29 11:30:27 +0800

    java

--- JavaSecurityWeb/src/main/java/servlet/secure/auth/DigestAuthServlet.java
+++ JavaSecurityWeb/src/main/java/servlet/secure/auth/DigestAuthServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import servlet.util.WebKeyUtil;
+
 /*
  * 
  * 為了實現HTTP摘要驗證，我們需要進一步的邏輯和某些額外的步驟。摘要驗證的主要優勢是它避免明文傳送密碼，而是發送一個摘要。
@@ -23,7 +25,23 @@ public class DigestAuthServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		final String REALM = "Restricted Area"; // 定義領域
+		// 檢查 client 端的 http header 中的 Authorization 標頭資訊
+		String authHeader = req.getHeader("Authorization");
+		// 檢查 'Authorization' 是否是以 'Digest' 開頭
+		if(authHeader != null && authHeader.startsWith("Digest ")) {
+			// 驗證 digest 的正確性
+			if(WebKeyUtil.isValidDigest(authHeader, req.getMethod(), req.getRequestURI(), REALM)) {
+				// 驗證成功
+				resp.getWriter().println("Welcome to the protected page (Digest) !");
+				return;
+			}
+		}
 		
+		// 如果驗證失敗, 則要向客戶端提供"摘要 Digest"認證資訊
+		resp.setHeader("WWW-Authenticate", WebKeyUtil.generateDigestChallenge(REALM));
+		// 發送 401 未授權的回應
+		resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 	
 }

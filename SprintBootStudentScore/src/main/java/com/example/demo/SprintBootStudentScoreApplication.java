90a06fdf303a90b5ec17e88fe60c460cdb92fa9e
aweit <aa4192696@gmail.com.tw>
2024-01-03 10:43:41 +0800

    java

---
+++ SprintBootStudentScore/src/main/java/com/example/demo/SprintBootStudentScoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootStudentScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootStudentScoreApplication.class, args);
+	}
+
+}
